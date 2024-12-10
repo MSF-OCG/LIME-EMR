39f2060db8836bdc9bc0cfb3cd6da25c89a7df71
Michaël Bontyes <michaelbontyes@gmail.com>
2024-12-10 15:56:15 +0100

    Adding custom labels for MH forms

--- e2e/features/filtered-answers.spec.ts
+++ e2e/features/filtered-answers.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 import { timeout } from '../../../playwright.config';
 
 test('test', async ({ page }) => {
-    await page.goto('http://lime-mosul-uat.madiro.org/openmrs/spa/patient/4aa1d485-5904-4b17-8bed-3affd38e95e7/chart/Patient%20Summary'); 
-    
+    await page.goto('http://lime-mosul-uat.madiro.org/openmrs/spa/patient/0f2851c6-62e1-4d42-932c-e094d621ef6a/chart/Patient%20Summary'); 
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await page.getByLabel('النماذج السريرية').click();
+    await page.getByText('MSF Mental Health - MHPSS Baseline').click();
 });
