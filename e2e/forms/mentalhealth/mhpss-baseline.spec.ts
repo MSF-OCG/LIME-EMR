2eee2d31c8f793042a2ee552db4bcd2b3f004d78
Pius Rubangakene <piruville@gmail.com>
2024-12-17 17:00:01 +0300

    e2e mhGap forms

--- e2e/forms/mentalhealth/mhpss-baseline.spec.ts
+++ e2e/forms/mentalhealth/mhpss-baseline.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { timeout } from '../../../playwright.config';
 
-test('test', async ({ page }) => {
+test('mhpss baseline form', async ({ page }) => {
   await page.goto('http://lime-mosul-uat.madiro.org/openmrs/spa/patient/4aa1d485-5904-4b17-8bed-3affd38e95e7/chart/Patient%20Summary');
   if (page.getByLabel('Start a visit')) {
     await page.getByLabel('Start a visit').click();
