9ed328c5fc61f57a49cfb82350d2044fbfea82a4
jnsereko <nserekojowashi@gmail.com>
2025-01-07 15:58:31 +0300

    Remove snakeYaml to maintain the string formart and assert presense of open user before creation

--- scripts/merge-openfn-yaml.groovy
+++ scripts/merge-openfn-yaml.groovy
@@ -1,63 +1,25 @@
-@Grab(group='org.yaml', module='snakeyaml', version='2.2')
-import org.yaml.snakeyaml.DumperOptions
-import org.yaml.snakeyaml.LoaderOptions
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.constructor.SafeConstructor
-import org.yaml.snakeyaml.representer.Representer
-import org.yaml.snakeyaml.nodes.Tag
-import org.yaml.snakeyaml.nodes.Node
-
+def distroOpenfnFile = "distro-project.yaml"
+def outputFileName = "openfn-project.yaml"
 def targetDir = "${project.build.directory}/${project.artifactId}-${project.version}/distro/configs/openfn"
-def outputFile = new File("${targetDir}/openfn-project.yaml")
-
-def loaderOptions = new LoaderOptions()
-def yaml = new Yaml(loaderOptions)
-
-def mergedYaml = [:]
-
-new File(targetDir).eachFileMatch(~/.+\.yaml$/) { file ->
-    file.withInputStream { stream ->
-        def yamlContent = yaml.load(stream)
-        if (yamlContent instanceof Map) {
-            yamlContent.each { key, value ->
-                if (mergedYaml.containsKey(key) && mergedYaml[key] instanceof Map && value instanceof Map) {
-                    mergedYaml[key] += value
-                } else {
-                    mergedYaml[key] = value
-                }
-            }
-        }
-    }
-}
-
-def dumperOptions = new DumperOptions()
-dumperOptions.setPrettyFlow(true)
-dumperOptions.setSplitLines(false)
-dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
-
-
-class CustomRepresenter extends Representer {
-    CustomRepresenter(DumperOptions options) {
-        super(options)
-    }
-
-    @Override
-    protected Node representScalar(Tag tag, String value, DumperOptions.ScalarStyle style) {
-        if (value.contains("\n")) {
-            value = value.replaceAll(/\r|\n/, ' ')
-            return super.representScalar(tag, value, style)
-        } else {
-            return super.representScalar(tag, value, style)
-        }
-    }
-}
-
-def representer = new CustomRepresenter(dumperOptions)
-def outputYaml = new Yaml(representer, dumperOptions)
-
+def outputFile = new File("${targetDir}/${outputFileName}")
 
 outputFile.withWriter { writer ->
-    outputYaml.dump(mergedYaml, writer)
+    writer.write("")
 }
 
-log.info("Merged OpenFn YAML files into ${outputFile.name}")
+def files = new File(targetDir).listFiles().findAll { it.name.endsWith('.yaml') }
+files.sort { it.name == "${distroOpenfnFile}" ? -1 : 0 }
+
+// Append the contents of each YAML file to the output file
+files.each { file ->
+    if (file != outputFile) {
+        // Add a comment header for each file
+        outputFile.append("# ${file.name} OpenFn configuration file\n")
+    }
+    
+    file.withReader { reader ->
+        outputFile.append(reader.text + "\n")
+    }
+}
+
+log.info("Appended YAML files into ${outputFile.name}")
--- scripts/run_msf_addons.sh
+++ scripts/run_msf_addons.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
 echo ""
-echo "$INFO Creating OpenFn admin user..."
 
-# Adding MSF admin account in a subshell to prevent stopping execution on error
-(
-    # create OpenFn user by default
-    docker exec -it $PROJECT_NAME-web-1 /app/bin/lightning eval \
-        'Lightning.Setup.setup_user(
-            %{
-                role: :superuser,
-                email: System.get_env("EMAIL_ADMIN"),
-                first_name: System.get_env("MSF_OPENFN_FIRST_NAME"),
-                last_name: System.get_env("MSF_OPENFN_LAST_NAME"),
-                password: System.get_env("MSF_OPENFN_PASSWORD")
-            },
-            System.get_env("SECRET_KEY_BASE"),
-            [
+# Check if MSF OpenFn super user exists in the database
+user_exists=$(docker exec $PROJECT_NAME-postgresql-1 bash -c "
+    psql -U \${OPENFN_DB_USER} -d \${OPENFN_DB_NAME} -t -c \"
+        SELECT * FROM users 
+        WHERE role = 'superuser' 
+        AND email = '\${EMAIL_ADMIN}' 
+        AND first_name = '\${MSF_OPENFN_FIRST_NAME}' 
+        AND last_name = '\${MSF_OPENFN_LAST_NAME}' 
+    \"
+")
+if [[ -n "$user_exists" ]]; then
+    echo "$INFO MSF OpenFn super user already exists. Skipping creation..."
+else
+    echo "$INFO MSF OpenFn super user does not exist. Proceeding with creation..."
+
+    # Adding MSF admin account in a subshell to prevent stopping execution on error
+    (
+        # create OpenFn user by default
+        docker exec -it $PROJECT_NAME-web-1 /app/bin/lightning eval \
+            'Lightning.Setup.setup_user(
                 %{
-                    name: "openmrs",
-                    schema: "raw",
-                    body: %{"username" => System.get_env("MSF_OPENMRS_USERNAME"), "instanceUrl" => System.get_env("MSF_OPENMRS_INSTANCE_URL"), "password" => System.get_env("MSF_OPENMRS_PASSWORD")}
+                    role: :superuser,
+                    email: System.get_env("EMAIL_ADMIN"),
+                    first_name: System.get_env("MSF_OPENFN_FIRST_NAME"),
+                    last_name: System.get_env("MSF_OPENFN_LAST_NAME"),
+                    password: System.get_env("MSF_OPENFN_PASSWORD")
                 },
-                %{
-                    name: "dhis2",
-                    schema: "raw",
-                    body: %{"username" => System.get_env("MSF_DHIS2_USERNAME"), "hostUrl" => System.get_env("MSF_DHIS2_HOST_URL"), "password" => System.get_env("MSF_DHIS2_PASSWORD")}
-                }
-            ]
-        )'  && echo "$INFO Creating OpenFn admin user completed successfully."
+                System.get_env("SECRET_KEY_BASE"),
+                [
+                    %{
+                        name: "openmrs",
+                        schema: "raw",
+                        body: %{"username" => System.get_env("MSF_OPENMRS_USERNAME"), "instanceUrl" => System.get_env("MSF_OPENMRS_INSTANCE_URL"), "password" => System.get_env("MSF_OPENMRS_PASSWORD")}
+                    },
+                    %{
+                        name: "dhis2",
+                        schema: "raw",
+                        body: %{"username" => System.get_env("MSF_DHIS2_USERNAME"), "hostUrl" => System.get_env("MSF_DHIS2_HOST_URL"), "password" => System.get_env("MSF_DHIS2_PASSWORD")}
+                    }
+                ]
+            )'  && echo "$INFO Creating OpenFn admin user completed successfully."
 
-) || echo "$ERROR Creating OpenFn admin user failed. Skipping"
-echo ""
+    ) || echo "$ERROR Creating OpenFn admin user failed. Skipping"
+    echo ""
 
-# this step requires internet. It downloads node modules. Should be run on the host
-echo "$INFO Pre-warming worker cache with adaptors..."
-docker exec -it $PROJECT_NAME-worker-1 sh -c "npm install -g @openfn/cli && openfn repo install  -a common@latest -a collections@latest -a http@6.5.1 -a openmrs@4.1.3 -a dhis2@5.0.1"
+    # this step requires internet. It downloads node modules. Should be run on the host
+    echo "$INFO Pre-warming worker cache with adaptors..."
+    docker exec -it $PROJECT_NAME-worker-1 sh -c "npm install -g @openfn/cli && openfn repo install  -a common@latest -a collections@latest -a http@6.5.1 -a openmrs@4.1.3 -a dhis2@5.0.1"
+fi
