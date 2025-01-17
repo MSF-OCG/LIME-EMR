ce6ab0370394eb0d9a9621869623e711b1866991
jnsereko <58003327+jnsereko@users.noreply.github.com>
2025-01-17 14:23:37 +0300

    run_msf_addons.sh should always run the worker cache step
    Since we don't currently have an actual directory on the server for storing this cache, we need to run this step on each installation and update.

--- scripts/run_msf_addons.sh
+++ scripts/run_msf_addons.sh
@@ -46,8 +46,8 @@ else
 
     ) || echo "$ERROR Creating OpenFn admin user failed. Skipping"
     echo ""
-
-    # this step requires internet. It downloads node modules. Should be run on the host
-    echo "$INFO Pre-warming worker cache with adaptors..."
-    docker exec -it $PROJECT_NAME-worker-1 sh -c "npm install -g @openfn/cli && openfn repo install  -a common@latest -a collections@latest -a http@6.5.1 -a openmrs@4.1.3 -a dhis2@5.0.1"
 fi
+
+# this step requires internet. It downloads node modules. Should be run on the host
+echo "$INFO Pre-warming worker cache with adaptors..."
+docker exec -it $PROJECT_NAME-worker-1 sh -c "npm install -g @openfn/cli && openfn repo install  -a common@latest -a collections@latest -a http@6.5.1 -a openmrs@4.1.3 -a dhis2@5.0.1"
